6c1f423585da4b89587cb7e05714edfc503f65f5
王亮 <wangliang1124@163.com>
2020-01-21 22:24:29 +0800

    function type, closure

--- Concentration/Concentration/Concentration.swift
+++ Concentration/Concentration/Concentration.swift
@@ -23,17 +23,20 @@ struct Concentration {
     
     private var indexOfOneAndOnlyFaceUpCard: Int? {
         get {
-            var foundIndex: Int?
-            for index in cards.indices {
-                if cards[index].isFaceUp {
-                    if foundIndex == nil {
-                        foundIndex = index
-                    } else {
-                        return nil
-                    }
-                }
-            }
-            return foundIndex
+            return  cards.indices.filter({cards[$0].isFaceUp}).onAndOnly
+            //            let faceUpCardIndices = cards.indices.filter({cards[$0].isFaceUp})
+            //            return faceUpCardIndices.count == 1 ? faceUpCardIndices.first : nil
+            //            var foundIndex: Int?
+            //            for index in cards.indices {
+            //                if cards[index].isFaceUp {
+            //                    if foundIndex == nil {
+            //                        foundIndex = index
+            //                    } else {
+            //                        return nil
+            //                    }
+            //                }
+            //            }
+            //            return foundIndex
         }
         set {
             for index in cards.indices {
@@ -68,4 +71,11 @@ struct Concentration {
     }
     
     
+}
+
+
+extension Collection {
+    var onAndOnly: Element? {
+        return count == 1 ? first : nil
+    }
 }
