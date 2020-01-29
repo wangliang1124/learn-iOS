991e368ec06de0dc6a7476eeb37510db1107419d
王亮 <wangliang1124@163.com>
2020-01-29 12:34:11 +0800

    Drag&Drop, tableView, collectionView

---
+++ EmojiArt/EmojiArt/EmojiCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by 王亮 on 2020/1/29.
+//  Copyright © 2020 王亮. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+}
