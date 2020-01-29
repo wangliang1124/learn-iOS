7b40b26987f2e177e3020903bf0135942f594837
王亮 <wangliang1124@163.com>
2020-01-29 23:06:36 +0800

    text field

---
+++ EmojiArt/EmojiArt/TextFieldCollectionViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  TextFieldCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by 王亮 on 2020/1/29.
+//  Copyright © 2020 王亮. All rights reserved.
+//
+
+import UIKit
+
+class TextFieldCollectionViewCell: UICollectionViewCell, UITextFieldDelegate {
+    
+
+    @IBOutlet weak var textField: UITextField! {
+        didSet {
+            textField.delegate = self
+        }
+    }
+    
+    var resignationHandler: (() -> Void)?
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        resignationHandler?()
+        textField.text = ""
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
