54d3008cf2237cd58b385938dfc81ef9ea0b27c5
Liang Wang <wangliang1124@163.com>
2022-07-28 16:20:40 +0800

    AnimateTableViewCell

---
+++ 30DaysOfSwift/AnimateTableViewCell/AnimateTableViewCell/ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  AnimateTableViewCell
+//
+//  Created by 王亮 on 2022/7/27.
+//
+
+import UIKit
+
+var tableData = ["Personal Life", "Buddy Company", "#30 days Swift Project", "Body movement training", "AppKitchen Studio", "Project Read", "Others", "Personal Life", "Buddy Company", "#30 days Swift Project", "Body movement training", "AppKitchen Studio", "Project Read", "Others"]
+
+
+class ViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "TableViewCell")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        animateTable()
+    }
+    
+    func animateTable() {
+//        self.tableView.reloadData()
+//        
+//        let cells = tableView.visibleCells
+//        let tableHeight: CGFloat = tableView.bounds.size.height
+//        
+//        for (index, cell) in cells.enumerated() {
+//            cell.transform = CGAffineTransform(translationX: 0, y: tableHeight)
+//            UIView.animate(withDuration: 1.0, delay: 0.05 * Double(index), usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: [], animations: {
+//                cell.transform = CGAffineTransform(translationX: 0, y: 0);
+//            }, completion: nil)
+//        }
+        self.tableView.reloadData()
+        
+        let cells = tableView.visibleCells
+        let tableHeight = tableView.bounds.size.height
+        
+        for (index, cell) in cells.enumerated() {
+            cell.transform = CGAffineTransform(translationX: 0, y: tableHeight)
+            UIView.animate(withDuration: 1.0, delay: 0.05 * Double(index), usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: [], animations: {
+                cell.transform = CGAffineTransform(translationX: 0, y: 0)
+            }, completion: nil)
+        }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath)
+        
+        cell.textLabel?.text = tableData[indexPath.row]
+        cell.textLabel?.textColor = .white
+        cell.textLabel?.backgroundColor = .clear
+        cell.textLabel?.font = UIFont(name: "Avenir Next", size: 18)
+        cell.selectionStyle = UITableViewCell.SelectionStyle.none
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.backgroundColor = colorForIndex(indexPath.row)
+    }
+    
+    func colorForIndex(_ index: Int) -> UIColor {
+        let itemCount = tableData.count - 1
+        let color = (CGFloat(index) / CGFloat(itemCount)) * 0.6
+        return UIColor(red: 1.0, green: color, blue: 0.0, alpha: 1.0)
+    }
+}
+
