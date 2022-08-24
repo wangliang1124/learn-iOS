1a785b4f83372f24a1a86ba8df9233eef81a883d
Liang Wang <wangliang1124@163.com>
2022-08-24 11:42:39 +0800

    test CABasicAnimation

--- 30DaysOfSwift/AnimatedSplash/AnimatedSplash/ViewController.swift
+++ 30DaysOfSwift/AnimatedSplash/AnimatedSplash/ViewController.swift
@@ -13,59 +13,76 @@ class ViewController: UIViewController {
 //        imageView.translatesAutoresizingMaskIntoConstraints = false
 //        return imageView
 //    }()
-    
-    @IBOutlet weak var imageView: UIImageView!
-    
+
+    @IBOutlet var imageView: UIImageView!
+
     var mask: CALayer = CALayer()
+    var maskView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         mask.contents = UIImage(named: "twitter")?.cgImage
         mask.contentsGravity = .resizeAspect
-        mask.bounds = CGRect(x: 0, y: 0, width: 100, height: 81)
+        mask.bounds = CGRect(x: 0, y: 0, width: 100, height: 100)
         mask.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-        
-        imageView.setNeedsLayout()
-        imageView.layoutIfNeeded()
-        
-        mask.position = CGPoint(x: imageView.frame.size.width / 2, y: imageView.frame.size.height / 2)
-        
+
+//        imageView.setNeedsLayout()
+//        imageView.layoutIfNeeded()
+
+        mask.position = view.center // CGPoint(x: imageView.frame.size.width / 2, y: imageView.frame.size.height / 2)
+
         imageView.layer.mask = mask
-        imageView.frame = view.bounds
-        
+
         view.addSubview(imageView)
-        
+
+        maskView = UIView(frame: view.frame)
+        maskView.backgroundColor = .white
+        view.addSubview(maskView)
+
+        if let scene = UIApplication.shared.connectedScenes.first,
+           let window = (scene.delegate as? SceneDelegate)?.window {
+            window.backgroundColor = UIColor(red: 29 / 255.0, green: 161 / 255.0, blue: 242 / 255.0, alpha: 1)
+        }
 
         animateMask()
     }
-    
+
     func animateMask() {
         let keyFrameAnimation = CAKeyframeAnimation(keyPath: "bounds")
-        
+
         keyFrameAnimation.delegate = self
         keyFrameAnimation.duration = 0.6
-        keyFrameAnimation.beginTime = CACurrentMediaTime() + 0.2
+        keyFrameAnimation.beginTime = CACurrentMediaTime() + 1
         keyFrameAnimation.timingFunctions = [CAMediaTimingFunction(name: .easeInEaseOut), CAMediaTimingFunction(name: .easeInEaseOut)]
-        
-//        do {
-//            keyFrameAnimation.fillMode = .forwards
-//            keyFrameAnimation.isRemovedOnCompletion = false
-//        }
-        
+
+        do {
+            keyFrameAnimation.fillMode = .forwards
+            keyFrameAnimation.isRemovedOnCompletion = false
+        }
+
         let initalBounds = NSValue(cgRect: mask.bounds)
-        let secondBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: 90 * 0.9, height: 73 * 0.9))
+        let secondBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: 85, height: 85))
         let finalBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: 1600, height: 1300))
-        
+
         keyFrameAnimation.values = [initalBounds, secondBounds, finalBounds]
         keyFrameAnimation.keyTimes = [0, 0.3, 1]
-        
+
         mask.add(keyFrameAnimation, forKey: "bounds")
+
+        let logoOpacityAnimation = CABasicAnimation(keyPath: "opacity")
+        logoOpacityAnimation.beginTime = CACurrentMediaTime() + 1
+        logoOpacityAnimation.duration = 1
+        logoOpacityAnimation.fromValue = 1
+        logoOpacityAnimation.toValue = 0
+        logoOpacityAnimation.delegate = self
+        maskView.layer.add(logoOpacityAnimation, forKey: "opacityAnimation")
     }
 }
 
 extension ViewController: CAAnimationDelegate {
     func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
-        self.imageView.layer.mask = nil
+        imageView.layer.mask = nil
+        maskView.removeFromSuperview()
     }
 }
